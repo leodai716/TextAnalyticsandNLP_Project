5a660d76ce777f5cd5c4f74492db61b4093417e4
marslyl <53827064+marslyl@users.noreply.github.com>
2019-10-08 01:26:39 +0800

    Add code-tweepy-scrap.py
    Tweepy-scrapping prototype.

---
+++ code-tweepy-scrap.py
@@ -0,0 +1,53 @@
+# This script shows how you can set up your Twitter API login
+# information.
+access_token = "1181129334968336384-q2TTL9ujFCfxHBfUxghKfR09JVQLaJ"
+access_token_secret = "Pa0Z5Zoo6O24DgnrxFh0RQstx65yx5aTTcWQQKK2p4u2I"
+consumer_key = "9eBNPCiIe7AExJxE0SvauYMDY"
+consumer_secret = "igPSn0FddQasy2LTPUHMzzUGW27s3VofInIdGpYxPY3LKhuLlS"
+import tweepy
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+# Check the tweets in your stream.
+class listener(tweepy.StreamListener):
+    def on_status(self, status):
+        with open('data-streaming-tweets.tsv', 'a', encoding = "utf-8") as f:
+            retweet = False 
+            extendedTweet = False
+            try:
+                refinedText = status.retweeted_status.extended_tweet['full_text']
+                retweet = True 
+                extendedTweet = True
+            except AttributeError:
+                try:
+                    refinedText = status.extended_tweet['full_text']
+                    extendedTweet = True
+                except AttributeError:
+                    refinedText = status.text
+                
+            #Print to console
+            print(status.user.screen_name, status.user.followers_count, status.created_at,\
+                  refinedText, retweet, extendedTweet, "\n", sep="\n")
+            
+            f.write(            # Write the data to file.
+                status.user.screen_name + '\t' + \
+                str(status.user.followers_count) + '\t' + \
+                str(status.created_at) + '\t' + \
+                refinedText + '\t' + \
+                str(retweet) + '\t' + \
+                str(extendedTweet) + '\n')
+    
+    def on_error(self, status_code):
+        print(status_code)
+
+# Instantiate an object of class `tweepy.Stream`.
+mystream = \
+    tweepy.Stream(
+        auth=api.auth,
+        listener=listener(),
+        tweet_mode = 'extended')
+# Filter the stream for whatever you want. In this case we are
+# listening for tweets that mention a select stock index/ETF, fixed
+# income index/ETF, or a commodities index/ETF. More tickers could be
+# added here.
+mystream.filter(languages=["en"], track=['Brexit'])
