70d28c00083d20f0a3ee58aade40a77aca94e08e
leodai716 <leodai@connect.hku.hk>
2019-10-25 20:26:47 +0800

    Update path

--- Twitter.bat
+++ Twitter.bat
@@ -1,4 +1,4 @@
 @echo on
 :startover
-"C:\Users\LeoDai\AppData\Local\Programs\Python\Python37\python.exe" "D:\Google Drive\HKU\Year 4 Sem 1\FINA4350 Text Analytics adn NLP in Finance\TextAnalyticsandNLP_Project\code-tweepy-scrape.py"
+"C:\Users\LeoDai\Anaconda3\python.exe" "D:\Google Drive\HKU\Year 4 Sem 1\FINA4350 Text Analytics adn NLP in Finance\TextAnalyticsandNLP_Project\code-tweepy-scrape.py"
 goto startover
