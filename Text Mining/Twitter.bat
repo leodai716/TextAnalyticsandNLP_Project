fdd57ece990a4de2e88fdcf613cb02b08d63028c
leodai716 <leodai@connect.hku.hk>
2019-10-26 11:16:20 +0800

    Reorganise depository

---
+++ Text Mining/Twitter.bat
@@ -0,0 +1,4 @@
+@echo on
+:startover
+"C:\Users\LeoDai\Anaconda3\python.exe" "D:\Google Drive\HKU\Year 4 Sem 1\FINA4350 Text Analytics adn NLP in Finance\TextAnalyticsandNLP_Project\Text Mining\code-tweepy-scrape.py"
+goto startover
--- Twitter.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-:startover
-"C:\Users\LeoDai\Anaconda3\python.exe" "D:\Google Drive\HKU\Year 4 Sem 1\FINA4350 Text Analytics adn NLP in Finance\TextAnalyticsandNLP_Project\code-tweepy-scrape.py"
-goto startover
