d3ea09b520093d8432de7623c170f234f5b84dd8
leodai716 <leodai@connect.hku.hk>
2019-11-21 11:57:48 +0800

    Remove cached

--- Text Mining/Twitter.bat
+++
@@ -1,4 +0,0 @@
-@echo on
-:startover
-"C:\Users\LeoDai\Anaconda3\python.exe" "D:\Google Drive\HKU\Year 4 Sem 1\FINA4350 Text Analytics and NLP in Finance\TextAnalyticsandNLP_Project\Text Mining\TweepyScrape.py"
-goto startover
